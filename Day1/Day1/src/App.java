14addcb5cc4537bcd54cf5ea8d23a9f42a6b7f68
Hubert Hartan <hubertyhartan@gmail.com>
2022-12-11 22:15:32 +1100

    Added part 2 of day 1

--- Day1/Day1/src/App.java
+++ Day1/Day1/src/App.java
@@ -1,18 +1,19 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
         int cal = 0;
         int maxCal = 0;
-    
+        List<Integer> calorieList = new ArrayList<>();//part2
 
         BufferedReader br = new BufferedReader(new FileReader("Day1/Day1/input/day1.txt"));
         String line;
         while((line=br.readLine())!=null){
             String lCal = line.trim();
             if(lCal.isBlank()){
-                
+                calorieList.add(cal);//part2
                 if(maxCal<cal){
                     
                     maxCal=cal;
@@ -26,7 +27,12 @@ public class App {
             
            
         }
+
+        
         System.out.println(maxCal);
+        calorieList.sort((a,b) -> b-a);
+
+        System.out.println(calorieList.get(0)+calorieList.get(1)+calorieList.get(2));
 
     }
 }
