a207227766b5ad5ec634d529c57b270b8e4790a4
Hubert Hartan <hubertyhartan@gmail.com>
2022-12-11 22:04:23 +1100

    first commit

---
+++ Day1/Day1/src/App.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int cal = 0;
+        int maxCal = 0;
+    
+
+        BufferedReader br = new BufferedReader(new FileReader("input/day1.txt"));
+        String line;
+        while((line=br.readLine())!=null){
+            String lCal = line.trim();
+            if(lCal.isBlank()){
+                
+                if(maxCal<cal){
+                    
+                    maxCal=cal;
+                }
+                cal =0;
+
+            }else{
+                cal+=Integer.parseInt(lCal);
+            }
+            
+            
+           
+        }
+        System.out.println(maxCal);
+
+    }
+}
