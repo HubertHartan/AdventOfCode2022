0042299bddb1f78cbfaee5c885eb9c4deb620125
Hubert Hartan <hubertyhartan@gmail.com>
2022-12-12 23:05:47 +1100

    day2 part 2 completed

--- Day2/Day2/src/App.java
+++ Day2/Day2/src/App.java
@@ -4,15 +4,31 @@ import java.io.FileReader;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        //A is rock
-        //B is paper
-        //C is scissors
-        //X is rock = 1
-        //Y is paper = 2
-        //Z is scissors = 3
-        //Win is 6 
-        //Draw is 3
-        //Lose is 0
+        /*
+            Part 1 Instructions:
+                A is rock
+                B is paper
+                C is scissors
+                X is rock = 1
+                Y is paper = 2
+                Z is scissors = 3
+                Win is 6 
+                Draw is 3
+                Lose is 0
+        */
+
+        /*
+            Part 2 Instructions:
+                A is rock
+                B is paper
+                C is scissors
+                Rock = 1
+                Paper = 2
+                Scissors = 3
+                X is lose = 0
+                Y is draw = 3
+                Z is win = 6
+        */
 
         BufferedReader br = new BufferedReader(new FileReader("Day2/Day2/input/day2.txt"));
         String line;
@@ -24,15 +40,15 @@ public class App {
             String lRPS = line.trim();
             switch(lRPS){
                 case "A X":
-                    points += 4;//draw and rock
-                    break;
-                case "A Y":
-                    points += 8;//win and paper
-                    break;
-                case "A Z":
                     points += 3;//lose and scissors
                     break;
-                ////
+                case "A Y":
+                    points += 4;//draw and rock
+                    break;
+                case "A Z":
+                    points += 8;//win and paper
+                    break;
+                ///////////////////////////////////////////////////////////////////////////
                 case "B X":
                     points += 1;//lose and rock
                     break;
@@ -42,15 +58,15 @@ public class App {
                 case "B Z":
                     points += 9;//win and scissors
                     break;
-                ////    
+                ///////////////////////////////////////////////////////////////////////////
                 case "C X":
-                    points += 7;//win and rock
-                    break;
-                case "C Y":
                     points += 2;//lose and paper
                     break;
-                case "C Z":
+                case "C Y":
                     points += 6;//draw and scissors
+                    break;
+                case "C Z":
+                    points += 7;//win and rock
                     break;
             }
             
