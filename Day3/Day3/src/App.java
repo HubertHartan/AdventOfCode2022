ea0aab7c9d3b3950371838964fecd717d868fda2
Hubert Hartan <hubertyhartan@gmail.com>
2022-12-14 23:12:31 +1100

    Day 3 part 2 completed

--- Day3/Day3/src/App.java
+++ Day3/Day3/src/App.java
@@ -9,31 +9,48 @@ public class App {
         int sum=0;
         Set<Character> characters1 = new TreeSet<Character>();
         Set<Character> characters2 = new TreeSet<Character>();
+        Set<Character> characters3 = new TreeSet<Character>();
+
+        int tracker = 1;
 
         while((line=br.readLine())!=null){
+            
             for(int i=0;i<line.length();i++){
-                //System.out.println(sum);
                 char c = line.charAt(i);
-                if(i<line.length()/2){
+                if(tracker%3==1){
+                  
                     characters1.add(c);
-                }else{
+                }else if(tracker%3==2){
+                    
                     characters2.add(c);
+                }else{
+                    
+                    characters3.add(c);
                 }
+              
+            }
+
+            
+            if(tracker%3==0){
+                characters1.retainAll(characters2);
+                characters1.retainAll(characters3);
+                Iterator val = characters1.iterator();
+    
+                char ch = (char) val.next();
                 
+                if(ch>96){
+                    sum+=(ch-96);
+                }else{
+                    sum+=(ch-38);
+                }
+
+                characters1.clear();
+                characters2.clear();
+                characters3.clear();
             }
-            characters1.retainAll(characters2);
-            Iterator val = characters1.iterator();
+            tracker++;
 
-            char ch = (char) val.next();
-
-            if(ch>96){
-                sum+=(ch-96);
-            }else{
-                sum+=(ch-38);
-            }
-
-            characters1.clear();
-            characters2.clear();
+            
             
         }
 
