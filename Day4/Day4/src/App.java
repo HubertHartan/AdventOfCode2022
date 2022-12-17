d7a7939d080aa2e31258fa796f4d92fafdde3166
Hubert Hartan <hubertyhartan@gmail.com>
2022-12-18 00:32:45 +1100

    day4 part 2 complete

--- Day4/Day4/src/App.java
+++ Day4/Day4/src/App.java
@@ -8,7 +8,8 @@ public class App {
         int start =0;
         int [] arr = new int[4];
         int idx=0;
-        int count=0;
+        int countOne=0;
+        int countTwo=0;
 
         while((line=br.readLine())!=null){
             for(int i=0;i<=line.length();i++){
@@ -18,20 +19,34 @@ public class App {
                     start=i+1;
                 }
             }
-            if(checkInRange(arr[0], arr[1], arr[2], arr[3])|| checkInRange(arr[2], arr[3], arr[0], arr[1])){
-                count++;
+            if(checkCntainRange(arr[0], arr[1], arr[2], arr[3])|| checkCntainRange(arr[2], arr[3], arr[0], arr[1])){
+                countOne++;
+            }
+
+            if(checkInRange(arr[0], arr[1], arr[2], arr[3])||checkInRange(arr[2], arr[3], arr[0], arr[1])){
+                countTwo++;
             }
 
             start=0;
             idx=0;
         }
         
-        System.out.println(count);
+        System.out.println(countOne);
+        System.out.println(countTwo);
+        br.close();
     }
 
     //Helper method to check if a range is in another range
-    public static boolean checkInRange(int lRng, int rRng, int lCnt, int rCnt ){
+    public static boolean checkCntainRange(int lRng, int rRng, int lCnt, int rCnt ){
         if(lRng<=lCnt && rRng>=rCnt){
+            return true;
+        }
+
+        return false;
+    }
+
+    public static boolean checkInRange(int lRng, int rRng, int lCnt, int rCnt ){
+        if((lCnt>=lRng && lCnt<=rRng) || (rCnt>=lRng && rCnt<=rRng)){
             return true;
         }
 
